5488b9f81213f9ccb642be217f2c5e0bcfba5e9b
Thomas Gaumont <thomas.gaumont@epitech.eu>
2017-03-07 17:59:16 +0000

    sphere norme

--- src/sphere.c
+++ src/sphere.c
@@ -5,10 +5,24 @@
 ** Login   <gaumon_t@epitech.net>
 **
 ** Started on  Mon Feb 20 12:28:30 2017 Gaumont Thomas
-** Last update Tue Mar  7 14:10:12 2017 Gaumont Thomas
+** Last update Tue Mar  7 17:53:39 2017 Gaumont Thomas
 */
 
 #include	"raytracer1.h"
+
+float		delta(float k1, float a, float b, float k)
+{
+  float		k2;
+
+  k1 = ((- b) + sqrt(k)) / (2 * a);
+  k2 = ((- b) - sqrt(k)) / (2 * a);
+  if (k1 < 0 && k2 < 0)
+    return (-1);
+  else if (k1 > k2)
+    return (k2);
+  else
+    return (k1);
+}
 
 float		intersect_sphere(sfVector3f eye_pos, sfVector3f dir_vector,
 				 float radius)
@@ -18,7 +32,6 @@ float		intersect_sphere(sfVector3f eye_pos, sfVector3f dir_vector,
   float		c;
   float		k;
   float		k1;
-  float		k2;
 
   a = pow(dir_vector.x, 2) + pow(dir_vector.y, 2) + pow(dir_vector.z, 2);
   b = 2 * ((eye_pos.x * dir_vector.x) + (eye_pos.y * dir_vector.y) +
@@ -34,16 +47,7 @@ float		intersect_sphere(sfVector3f eye_pos, sfVector3f dir_vector,
       return (k1);
     }
   else if (k > 0)
-    {
-      k1 = ((- b) + sqrt(k)) / (2 * a);
-      k2 = ((- b) - sqrt(k)) / (2 * a);
-      if (k1 < 0 && k2 < 0)
-	return (-1);
-      else if (k1 > k2)
-	return (k2);
-      else
-	return (k1)
-    }
+    return (delta(k1, a, b, k));
   return (-1);
 }
 
